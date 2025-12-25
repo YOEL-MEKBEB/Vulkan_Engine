8bd16cf66c302b37ce89effed3b87ecdb08022b8
Yoel Mekbeb <yoelmekbeb10@gmail.com>
2025-12-25 14:08:13 -0600

    updated fastgltf

---
+++ src/vk_loader.cpp
@@ -0,0 +1,12 @@
+#include "stb_image.h"
+#include <iostream>
+#include <vk_loader.h>
+
+#include "vk_engine.h"
+#include "vk_initializers.h"
+#include "vk_types.h"
+#include <glm/gtx/quaternion.hpp>
+
+#include <fastgltf/glm_element_traits.hpp>
+#include <fastgltf/parser.hpp>
+#include <fastgltf/tools.hpp>
